b5126c48f968382ce799172379127f4c6e1fd089
lrush85 <lance.rush1@gmail.com>
2019-04-08 08:54:45 -0500

    Added data for all precreated files, moved index file into public folder

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,94 @@
-$(document).ready(function(){
-    alert("You hit main.js")
-})
+const player = document.getElementById('player');
+const canvas = document.getElementById('canvas');
+const captureButton = document.getElementById('capture');
+const fileInput = document.getElementById('file-input');
+const context = canvas.getContext('2d');
+const video = document.querySelector('video');
+
+var constraints = {
+    audio: false,
+    video: {
+        width: {
+            min: 640,
+            ideal: 1280,
+            max: 1920
+        },
+        height: {
+            min: 480,
+            ideal: 720,
+            max: 1080
+        },
+        facingMode: "environment" //Defualt direct of the camera for mobile devices.
+    }
+};
+
+// ================================================
+//                                  FUNCTIONS
+// ================================================
+
+
+
+
+
+// Media Devices
+
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(function (stream) {
+        var videoTracks = stream.getVideoTracks();
+        video.srcObject = stream;
+    })
+    .catch(function (err) {
+        console.log(err.name + ": " + err.message);
+    });
+
+// Attach the video stream to the video element and autoplay.
+navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+        player.srcObject = stream;
+    });
+
+document.querySelector("#capture").addEventListener("click", function (event) {
+    if (video) {
+        canvas.width = video.clientWidth;
+        canvas.height = video.clientHeight;
+        var context = canvas.getContext("2d");
+        context.drawImage(video, 0, 0);
+    }
+});
+
+var maxwidth = 100;
+var maxheigth = 100;
+
+$(captureButton).on('click', function (event) {
+    // Draw the video frame to the canvas.
+    console.log(this);
+    context.drawImage(player, 0, 0, canvas.maxwidth, canvas.maxheight);
+
+
+
+
+
+
+
+
+
+    // Stop all video streams.
+    player.srcObject.getVideoTracks().forEach(track => track.stop());
+
+
+
+
+
+    // image = canvas.toDataURL('image/jpeg', 1.0);
+
+    // $.ajax({
+    //         type: "POST",
+    //         url: "/user/image",
+    //         data: {
+    //             img: image
+    //         }
+    //     }).done(function (result) {
+    //             console.log('saved: ' + result);
+    //     });
+
+});
--- server.js
+++ server.js
@@ -1,41 +1,62 @@
-require("dotenv").config();
 var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var db = require("./models");
+const fs = require('fs');
+const vision = require('@google-cloud/vision');
+const bodyParser = require("body-parser");
+const base64ToImage = require("base64-to-image");
+const path = require("path");
 var app = express();
-var PORT = process.env.PORT || 3000;
 
-app.set("views", path.join(__dirname, "views"));
+var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
-app.use(
-  express.urlencoded({
-    extended: false
-  })
-);
+// Sets up the Express app to handle data parsing
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
-app.use(express.static("public"));
 
-var syncOptions = {
-    force: false
-  };
-  
-  // If running a test, set syncOptions.force to true
-  // clearing the `testdb`
-  if (process.env.NODE_ENV === "test") {
-    syncOptions.force = true;
-  }
-  
-  // Starting the server, syncing our models ------------------------------------/
-  db.sequelize.sync(syncOptions).then(function() {
-    app.listen(PORT, function() {
-      console.log(
-        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-        PORT,
-        PORT
-      );
-    });
+
+app.use(express.static('public'));
+// require("./app.js")(app);
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+app.get("/user/image", function (req, res) {
+});
+
+
+app.post("/user/image", function (req, res) {
+
+
+  var image = req.body;
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient({
+    keyFilename: "APIkey.json"
   });
-  
-  module.exports = app;
+
+  // Performs label detection on the image file
+  client
+    .textDetection()
+    .then(results => {
+      console.log(results[0].fullTextAnnotation.text);
+
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+
+});
+
+
+
+
+
+
+// =============================================================================
+// SERVER
+// =============================================================================
+
+app.listen(PORT, function () {
+  console.log("App listening on http://localhost:" + PORT);
+});
